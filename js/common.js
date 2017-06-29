97955c995595ee5b99ade6b733a1f6cffd98ebe3
于文强 <741369024@qq.com>
2017-06-30 00:34:50 +0800

    用画布 写的 绘画板‘

--- js/common.js
+++ js/common.js
@@ -2,52 +2,146 @@
 
 var style = 0;
 var timer;
-var lock = false;
+var lock0 = false;
+var lock2 = false;
+var mycolor = "black";
 $(function(){
 	var c = document.getElementById("c");
 	var ctx = c.getContext('2d');
+	var c2 = document.getElementById("c2");
+	var ctx2 = c.getContext('2d');
 	ctx.beginPath();
 	ctx.rect(0,0,800,600);
 	ctx.stroke();
-	$("#juxing").click(function(){
-		console.log("我是矩形");
-		style=1;
-		$("#juxing").css("border","2px solid black");
-	})
-	$("#c").mousedown(function(){
-		lock = true;
-		var beginx =event.pageX;
-		var beginy =event.pageY;
-		console.log("我点下来的时候：",beginx,beginy);
-		$("#c").mousemove(function(event){
-			if(lock)
-			{
-			
-				console.log("鼠标移动的时候",event.pageX + ", " + event.pageY);
-				ctx.beginPath();
-				console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
-				ctx.moveTo(beginx,beginy);
-				ctx.lineTo(event.pageX,event.pageY);
-				ctx.stroke();
-				
-			
-	    	}else{
-	    		
-	    	}
-	    	beginx =event.pageX;
-			beginy =event.pageY;
-		})
-	})
-		
-	$("#c").mouseup(function(){
 	
-		lock=false;
+	ctx2.beginPath();
+	ctx2.rect(0,0,800,600);
+	ctx2.stroke();
+	
+    ctx2.clearRect(0,0,200,200);
+	ctx.beginPath();
+	ctx.moveTo(10,10);
+	ctx.lineTo(100,100);
+	ctx.stroke();
+	ctx2.beginPath();
+	ctx2.moveTo(0,20);
+	ctx2.lineTo(100,100);
+	ctx2.stroke();
+	//添加选中样式
+	$("li").click(function(){
+		$(".choice").removeClass("choice");
+		$(this).addClass("choice");
+	})
+	
+	$("#qianbi").click(function(){
+		style=0;
+	})
+	$("#juxing").click(function(){
+		style=1;
+	})
+	$("#yuanxing").click(function(){
+		style=2;
+	})
+	$("#xiangpi").click(function(){
+		style=3;
+	})
+	$(".red").click(function(){
+		mycolor = "red";
+	})
+	$(".black").click(function(){
+		mycolor = "black";
+	})
+	$(".white").click(function(){
+		mycolor = "white";
+	})
+	$(".blue").click(function(){
+		mycolor = "blue";
+	})
+	var beginx;
+	var beginy;
+	$("#div1").mousedown(function(event){
+		beginx =event.pageX;
+		beginy =event.pageY;
+		ctx2.beginPath();
+		switch(style)
+		{
+			//铅笔状态
+			case 0:
+				console.log("我选的是铅笔");
+				lock0 = true;
+				//console.log("我点下来的时候：",beginx,beginy);
+				$("#div1").mousemove(function(event){
+					if(lock0)
+					{
+					
+	//					console.log("鼠标移动的时候",event.pageX + ", " + event.pageY);
+						ctx.beginPath();
+	//					console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
+						ctx.moveTo(beginx,beginy);
+						ctx.lineTo(event.pageX,event.pageY);
+						ctx.strokeStyle = mycolor;
+						ctx.stroke();
+						beginx =event.pageX;
+						beginy =event.pageY;
+			    }
+				})
+				break;
+			case 1:
+				console.log("画个矩形吧！");
+				break;
+			case 2:
+				lock2=true;
+				console.log("画个圆吧 ！")
+				console.log("我画圆的时候：",beginx,beginy);
+				ctx2.save();
+				$("#div1").mousemove(function(event){
+					
+					if(lock2)
+					{
+						//console.log("鼠标移动的时候",event.pageX + ", " + event.pageY);
+						
+//						ctx2.height = ctx2.height;
+//						ctx2.clearRect(0,0,800,600);  
+//						ctx2.beginPath();
+//						ctx2.rect(0,0,800,600);
+//						ctx2.strokeStyle ="black";
+//						ctx2.stroke();
+//						ctx2.save();
+
+						console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
+						xx=event.pageX-beginx;
+						yy=event.pageY-beginy;
+						console.log(event.pageX,event.pageY);
+						r = Math.sqrt(xx*xx+yy*yy)/2;
+						centerx = (event.pageX+beginx)/2;
+						centery = (event.pageY+beginy)/2;
+						console.log(xx,yy,centerx,centery,r);
+						ctx2.arc(centerx,centery,r,0,2*Math.PI,true);
+						ctx2.strokeStyle = mycolor;
+						ctx2.stroke();
+//						
+			    	}
+					ctx2.restore();
+	//		    	beginx =event.pageX;
+	//				beginy =event.pageY;
+				})
+//				ctx2.restore();
+
+				
+				break;
+		}
 		
+	})
+		
+	$("#div1").mouseup(function(){
+	
+		lock0=false;
+		lock2 = false;
 	});
 })
 
 function mydraw(){
-	$("#c").mousemove(function(event)
+	$("#div1").mousemove(function(event)
 	{
 		console.log(event.pageX + ", " + event.pageY);
 	})
---
+++ js/common2.js
@@ -0,0 +1,207 @@
+
+var style = 0;
+var mycolor = "black";
+var mywidth = 2.5;
+var beginx;
+var beginy;
+$(function(){
+	var c = document.getElementById("c");
+	var ctx = c.getContext('2d');
+	var c2 = document.getElementById("c2");
+	var ctx2 = c2.getContext('2d');
+	ctx.beginPath();
+	ctx.rect(0,0,800,600);
+	ctx.stroke();
+
+	//添加选中样式
+	$("li").click(function(){
+		$(".cleardraw").removeClass("choice");
+		$(this).parent().children().removeClass("choice");
+		$(this).addClass("choice");
+	})
+	$(".small").click(function(){
+		mywidth = 1;
+	})
+	$(".normol").click(function(){
+		mywidth = 2.5;
+	})
+	$(".big").click(function(){
+		mywidth = 5;
+	})
+	$("#qianbi").click(function(){
+		style=0;
+	})
+	$("#juxing").click(function(){
+		style=1;
+	})
+	$("#yuanxing").click(function(){
+		style=2;
+	})
+	$("#xiangpi").click(function(){
+		style=3;
+	})
+	$(".red").click(function(){
+		mycolor = "red";
+	})
+	$(".black").click(function(){
+		mycolor = "black";
+	})
+	$(".white").click(function(){
+		mycolor = "white";
+	})
+	$(".yellow").click(function(){
+		mycolor = "yellow";
+	})
+	$(".blue").click(function(){
+		mycolor = "blue";
+	})
+	$(".cleardraw").click(function(){
+		ctx.clearRect(0,0,800,600);
+		ctx.beginPath();
+		ctx.rect(0,0,800,600);
+		ctx.strokeStyle = "black"
+		ctx.lineWidth = "1"
+		ctx.stroke();
+	})
+	
+	$("#div1").mousedown(function(event){
+		beginx =event.pageX;
+		beginy =event.pageY;
+		console.log("我点下来的时候：",beginx,beginy);
+		switch(style)
+		{
+			//铅笔状态
+			case 0:
+				console.log("我选的是铅笔");
+				lock0 = true;
+				//console.log("我点下来的时候：",beginx,beginy);
+				$("#div1").mousemove(function(event){
+					if(lock0)
+					{			
+	//					console.log("鼠标移动的时候",event.pageX + ", " + event.pageY);
+						ctx.beginPath();
+	//					console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
+						ctx.moveTo(beginx,beginy);
+						ctx.lineTo(event.pageX,event.pageY);
+						ctx.strokeStyle = mycolor;
+						ctx.lineWidth = mywidth;
+						ctx.stroke();
+						beginx =event.pageX;
+						beginy =event.pageY;
+			 	   }
+				})
+				break;
+			case 1:
+				lock1 = true;
+				console.log("画个矩形吧！");
+				$("#div1").mousemove(function(event){			
+					if(lock1)
+					{
+						ctx2.clearRect(0,0,800,600); 
+						xx=event.pageX-beginx;
+						yy=event.pageY-beginy;
+						ctx2.beginPath();
+						ctx2.rect(beginx,beginy,xx,yy);
+						ctx2.strokeStyle = mycolor;
+						ctx2.lineWidth = mywidth;
+						ctx2.stroke();
+				    }
+				})
+				break;
+			case 2:
+				lock2=true;
+				console.log("画个圆吧 ！")
+//				console.log("我画圆的时候：",beginx,beginy);
+				$("#div1").mousemove(function(event){			
+					if(lock2)
+					{
+						ctx2.clearRect(0,0,800,600); 					
+						console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
+						xx=event.pageX-beginx;
+						yy=event.pageY-beginy;
+						console.log(event.pageX,event.pageY);
+						r = Math.sqrt(xx*xx+yy*yy)/2;
+						centerx = (event.pageX+beginx)/2;
+						centery = (event.pageY+beginy)/2;
+						console.log(xx,yy,centerx,centery,r);
+						ctx2.beginPath();
+						ctx2.arc(centerx,centery,r,0,2*Math.PI,true);
+						ctx2.strokeStyle = mycolor;
+						ctx.lineWidth = mywidth;
+						ctx2.stroke();
+				    }
+				})
+				break;
+			//橡皮状态
+			case 3:
+				console.log("我选的是橡皮");
+				lock3 = true;
+				//console.log("我点下来的时候：",beginx,beginy);
+				$("#div1").mousemove(function(event){
+					if(lock3)
+					{
+					
+	//					console.log("鼠标移动的时候",event.pageX + ", " + event.pageY);
+						ctx.beginPath();
+	//					console.log("鼠标移动的时候beginx和beginy",beginx,beginy);
+						ctx.moveTo(beginx,beginy);
+						ctx.lineTo(event.pageX,event.pageY);
+						ctx.strokeStyle = "white";
+						ctx.lineWidth = mywidth*3;
+						ctx.stroke();
+						beginx =event.pageX;
+						beginy =event.pageY;
+			   		 }
+				})
+				break;
+		}
+		
+	})
+		
+	$("#div1").mouseup(function(){
+		switch(style)
+		{
+			//铅笔状态
+			case 0:
+				
+				break;
+			//矩形状态
+			case 1:
+				ctx2.clearRect(0,0,800,600); 
+				xx=event.pageX-beginx;
+				yy=event.pageY-beginy;
+				ctx.beginPath();
+				ctx.rect(beginx,beginy,xx,yy);
+				ctx.strokeStyle = mycolor;
+				ctx.lineWidth = mywidth;
+				ctx.stroke();
+				break;
+			//画圆状态
+			case 2:
+				ctx2.clearRect(0,0,800,600); 
+				xx=event.pageX-beginx;
+				yy=event.pageY-beginy;
+				console.log(event.pageX,event.pageY);
+				r = Math.sqrt(xx*xx+yy*yy)/2;
+				centerx = (event.pageX+beginx)/2;
+				centery = (event.pageY+beginy)/2;
+				console.log(xx,yy,centerx,centery,r);
+				ctx.beginPath();
+				ctx.arc(centerx,centery,r,0,2*Math.PI,true);
+				ctx.strokeStyle = mycolor;
+				ctx.lineWidth = mywidth;
+				ctx.stroke();
+				break;
+			case 3:
+				
+				break;
+				
+		}
+		
+		lock0=false;
+		lock1=false;
+		lock2=false;
+		lock3=false;
+	});
+})
+
